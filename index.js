ccf66578c0f9c9e0ff1ba0edbfdfd79e55329609
ayush050501 <130683128+ayush050501@users.noreply.github.com>
2024-08-24 00:52:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT
  
-app.use(cors())
+app.use(cors());
+app.use(cors({
+     origin: 'http://localhost:3000', // Allow only your frontend origin
+     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+   }));
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
